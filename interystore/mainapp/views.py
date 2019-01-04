89f56b9aa858c48f075389902fa06ea63dea2090
kulakov <kdkulakov@gmail.com>
2019-01-04 19:10:00 +0300

    Add django project + create template

---
+++ interystore/mainapp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+
+def main(request):
+    return render(request, 'mainapp/index.html')
+
+def products(request):
+    return render(request, 'mainapp/products.html')
+
+def detail(request):
+    return render(request, 'mainapp/detail.html')
+
+def contacts(request):
+    return render(request, 'mainapp/contacts.html')
